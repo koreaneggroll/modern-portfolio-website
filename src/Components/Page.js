0d10fe0d6c08eaa92a5b0e35dc90cb75bb77aeb9
koreaneggroll <aringrigoras@gmail.com>
2022-04-19 23:08:46 +0300

    made things more modular

--- src/Components/Page.js
+++ src/Components/Page.js
@@ -4,20 +4,11 @@ import {
   Box,
   theme,
   Flex,
-  Spacer,
-  Circle,
-  Center,
-  Divider,
-  Text,
-  IconButton
 } from '@chakra-ui/react';
 
-import { ChevronRightIcon, TriangleDownIcon } from '@chakra-ui/icons'
 
-import { ColorModeSwitcher } from '../ColorModeSwitcher';
 
-import Loader from './Loader';
-import Aboutme from './AboutMe';
+import TopBar from './TopBar';
 import Main from './Main';
 import "../Css/Page.css";
 
@@ -37,56 +28,7 @@ function Page() {
         //<Loader />
         }
         <Box className="Page-wrapper">
-            <Flex className="topbar" m={6} >
-                <Center className="logo">
-                    Logo
-                </Center>
-
-                <Center display={{base: 'none', md: 'flex', lg: 'flex'}} color="gray.600">
-                    <Box ml={{base: '0', md: '2', lg: '20'}}>
-                        <ChevronRightIcon />
-                    </Box>
-
-                    <Box ml={{base: '0', md: '2', lg: '20'}}>
-                        <i>
-                            aringrigoras@gmail.com
-                        </i>
-                    </Box>
-                </Center>
-
-                <Spacer/>
-
-                <Center className="icons">
-                    <Circle _hover={{color: 'gray'}} mr={{base: '4', md: '8', lg: '10'}} className="icon">
-                        {
-                            //use icon button
-                        }
-                        Icon
-                    </Circle>
-
-
-                    <Circle _hover={{color: 'gray'}} mr={{base: '4', md: '8', lg: '10'}} className="icon">
-                        Icon
-                    </Circle>
-
-
-                    <Circle _hover={{color: 'gray'}} mr={{base: '4', md: '8', lg: '10'}} className="icon">
-                        Icon
-                    </Circle>
-
-
-                    <Circle _hover={{color: 'gray'}} mr={{base: '4', md: '8', lg: '10'}} className="icon">
-                        Icon
-                    </Circle>
-
-
-                    <ColorModeSwitcher />
-                </Center>
-
-            </Flex>
-
-            <Divider orientation='horizontal'/>
-
+            <TopBar />
 
             <Flex className="main">
                 <Main />
---
+++ src/Components/TopBar.js
@@ -0,0 +1,75 @@
+import React from 'react';
+
+import {
+    Box,
+    Flex,
+    Spacer,
+    Circle,
+    Center,
+    Divider,
+  } from '@chakra-ui/react';
+
+
+import { ChevronRightIcon, TriangleDownIcon } from '@chakra-ui/icons'
+
+import { ColorModeSwitcher } from '../ColorModeSwitcher';
+
+function TopBar(){
+    return(
+        <>
+            <Flex className="topbar" m={6} >
+                <Center className="logo">
+                    Logo
+                </Center>
+
+                <Center display={{base: 'none', md: 'flex', lg: 'flex'}} color="gray.600">
+                    <Box ml={{base: '0', md: '2', lg: '20'}}>
+                        <ChevronRightIcon />
+                    </Box>
+
+                    <Box ml={{base: '0', md: '2', lg: '20'}}>
+                        <i>
+                            aringrigoras@gmail.com
+                        </i>
+                    </Box>
+                </Center>
+
+                <Spacer/>
+
+                <Center className="icons">
+                    <Circle _hover={{color: 'gray'}} mr={{base: '4', md: '8', lg: '10'}} className="icon">
+                        {
+                            //use icon button
+                        }
+                        Icon
+                    </Circle>
+
+
+                    <Circle _hover={{color: 'gray'}} mr={{base: '4', md: '8', lg: '10'}} className="icon">
+                        Icon
+                    </Circle>
+
+
+                    <Circle _hover={{color: 'gray'}} mr={{base: '4', md: '8', lg: '10'}} className="icon">
+                        Icon
+                    </Circle>
+
+
+                    <Circle _hover={{color: 'gray'}} mr={{base: '4', md: '8', lg: '10'}} className="icon">
+                        Icon
+                    </Circle>
+
+
+                    <ColorModeSwitcher />
+                </Center>
+
+            </Flex>
+
+            <Divider orientation='horizontal'/>
+
+        </>
+    )
+}
+
+
+export default TopBar;
