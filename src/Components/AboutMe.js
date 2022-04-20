fd15a3c71600926bf206b7ad2753e63d353a0606
koreaneggroll <aringrigoras@gmail.com>
2022-04-20 18:49:25 +0300

    refactoring the about me page

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -23,7 +23,7 @@ import TopBar from './TopBar';
 
 import AboutmeSVG from '../Pictures/about-page.svg';
 
-import { ArrowRightIcon } from '@chakra-ui/icons';
+import { ArrowRightIcon, ChevronDownIcon } from '@chakra-ui/icons';
 
 
 function Aboutme(){
@@ -36,8 +36,8 @@ function Aboutme(){
                 </div>
 
 
-                <div className="main-about-me">
-                    <Box className="about-me-wrapper">
+                    {/*
+                        <Box className="about-me-wrapper">
                         <Box mt={{base: '20', md: '30', lg: '40'}} display={{base: 'block', md: 'flex', lg: 'flex'}} className="about-me-heading">
                             <Box  ml={{base: '20', md: '10', lg: '10'}}>
                                 <Text display={{base: 'block', md: 'flex', lg: 'flex'}} fontSize={{base: '80', md: '80', lg: '100'}}>
@@ -68,11 +68,47 @@ function Aboutme(){
                                     <ArrowRightIcon fontSize={40} />
                                 </Box>
                             </Show>
+
+
+
+                           <Show below='sm'>
+                                <Center>
+                                    <Box mt={20} color='gray.500' className="small-intro-icon">
+                                        <ChevronDownIcon />
+                                    </Box>
+                                </Center>
+                            </Show>
+
+
+                            <Show above='sm'>
+                                <Box ml={{md: '300', lg: '500'}} color='gray.500' className="small-intro-icon">
+                                    <ChevronDownIcon />
+                                </Box>
+                            </Show>
+                            
                         </Box>
 
                         
+                        
                     </Box>
-                </div>
+
+    */}  
+
+
+            <Box display="block" className="about-me-wrapper">
+                <Center>
+                    <Text mt={20} className="about-text-header">
+                        {
+                            //make it underlined
+                        }
+                        About
+                    </Text>
+                </Center>
+
+
+                
+            </Box>
+                    
 
 
 
