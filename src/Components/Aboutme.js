acecdf0f4528129268e91f60f78906ae32feebcc
koreaneggroll <aringrigoras@gmail.com>
2022-04-27 16:20:28 +0300

    made the scss files

--- src/Components/Aboutme.js
+++ src/Components/Aboutme.js
@@ -19,26 +19,28 @@ function Aboutme(){
 
       <Box ml={{base: '0', md: '0', lg: '0'}} mr={{base: '0', md: '300', lg: '600'}} className="about-me-wrapper">
           <Center>
-            <Box display="block" className="about-me">
+            <Box className="about-me">
 
                 <Box className="about-me-header">
 
                     <Flex className="about-header">
-                        <p className="number">
-                            01. 
-                        </p>
 
                         <p className="header">
                             <b>
                                 About me
+                                <span className="line">
+
+                                </span>
                             </b>
                         </p>
                     </Flex>
                 </Box>
 
 
-                <Box className="about-me-paragraph">
-
+                <Box maxW={{lg: "400"}} className="about-me-paragraph">
+                    <p>
+                        
+                    </p>
                 </Box>
 
 
