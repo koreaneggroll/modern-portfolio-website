22479bcb4f75788b193c92afff1a576f954df643
koreaneggroll <aringrigoras@gmail.com>
2022-05-30 23:39:50 +0300

    about me phoot

--- src/Components/Aboutme.js
+++ src/Components/Aboutme.js
@@ -5,10 +5,18 @@ import {
     Box,
     Center,
     Flex,
+    Show,
+    Divider,
+    Image,
+    Spacer
 } from '@chakra-ui/react';
 
 
 import "../Css/Aboutme.css";
+
+
+
+import aboutmephoto from '../Pictures/arin-logo-new.PNG';
 
 
 function Aboutme(){
@@ -17,9 +25,9 @@ function Aboutme(){
 
         <br/><br/><br/><br/>
 
-      <Box ml={{base: '10', md: '10', lg: '0'}} mr={{base: '10', md: '300', lg: '600'}} className="about-me-wrapper">
+      <Box ml={{base: '10', md: '100', lg: '200'}} mr={{base: '10', md: '300', lg: '600'}} className="about-me-wrapper">
           <Center>
-            <Box className="about-me">
+            <Box ml={{base: '10', md: '20', lg: '30'}} className="about-me">
 
                 <Box className="about-me-header">
 
@@ -36,19 +44,33 @@ function Aboutme(){
                     </Flex>
                 </Box>
 
+                <Flex>
+                    <Box ml={{base: '10', md: "10", lg: "5"}} maxW={{lg: "500"}} className="about-me-paragraph">
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elit ut aliquam purus sit amet luctus. Enim praesent elementum facilisis leo vel fringilla. Nulla pharetra diam sit amet nisl. In fermentum posuere urna nec tincidunt praesent semper feugiat. Justo eget magna fermentum iaculis eu non diam. Feugiat sed lectus vestibulum mattis ullamcorper velit sed. Eget aliquet nibh praesent tristique magna sit. Enim tortor at auctor urna nunc id. Faucibus et molestie ac feugiat sed lectus vestibulum mattis ullamcorper.
+                        </p>
+                        
+                    </Box>
 
-                <Box ml={{base: '10', md: "10", lg: "5"}} maxW={{lg: "500"}} className="about-me-paragraph">
-                    <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elit ut aliquam purus sit amet luctus. Enim praesent elementum facilisis leo vel fringilla. Nulla pharetra diam sit amet nisl. In fermentum posuere urna nec tincidunt praesent semper feugiat. Justo eget magna fermentum iaculis eu non diam. Feugiat sed lectus vestibulum mattis ullamcorper velit sed. Eget aliquet nibh praesent tristique magna sit. Enim tortor at auctor urna nunc id. Faucibus et molestie ac feugiat sed lectus vestibulum mattis ullamcorper.
-                    </p>
-                </Box>
+
+                    <Box className="divider">
+                        <Divider ml={40} mr={40} orientation="vertical"/>
+                    </Box>
 
 
+                    <Show above="sm">
+                        <Box mb={{md: '10', lg: '20'}} className="about-me-photo">
+                            
+                        </Box>
+                    </Show>
 
-                <Box className="about-me-photo">
-                    
-                </Box>
 
+                    <Show below="sm">
+                        <Box clasName="about-me-photo-mobile" >
+                            
+                        </Box>
+                    </Show>
+                </Flex>
             </Box>
         </Center>
       </Box>
