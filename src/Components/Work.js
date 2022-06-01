6ba638fec956f20b21eb8c97e520adcee2bece24
koreaneggroll <aringrigoras@gmail.com>
2022-06-02 00:33:53 +0300

    Work tablist

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -2,14 +2,26 @@ import React from 'react';
 
 
 import { 
-    Box
+    Box,
+    Tab,
+    TabList,
+    Tabs,
+    TabPanel,
+    TabPanels
 } from '@chakra-ui/react';
 
 
 function Work(){
     return(
         <Box className="Work">
-            Work
+            <Box className="work-wrapper">
+                <Tabs variant="soft-rounded" colorScheme='teal' className="work-tabs">
+                    <TabList>
+                        <Tab>Fiverr</Tab>
+                        <Tab>N/A</Tab>
+                    </TabList>
+                </Tabs>
+            </Box>
         </Box>
     );
 };
